b34e60e1ea03526e1de21ea7038ae40fc980115b
mjsuser14 <meghasalunke2003@gmail.com>
2025-12-30 23:56:43 +0530

    Added Day2 file

---
+++ Day2.py
@@ -0,0 +1,9 @@
+#Day2- Challenge 2 Variables and Data types
+# Calculate the area of a rectangle using user-input length and width
+l = float(input("Enter the length of Rectangle:"))
+w = float(input("Enter the width of Rectangle:"))
+
+area = l*w
+print("The Area of Rectangle is:",area)
+
+
